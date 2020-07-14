13a68631385d9f194eb6352699c404d296215d31
Franklin Bado <fbado66@gmail.com>
2020-07-14 03:58:44 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,11 +1,11 @@
 # Write your code here
 
-count = 10
-while count > 0 
-puts count 
-count = count - 1 
-end 
-puts "Happy New Year!"
+# count = 10
+# while count > 0 
+# puts count 
+# count = count - 1 
+# end 
+# puts "Happy New Year!"
 
 
 count = 10 
