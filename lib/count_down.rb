46a8eed0a12746483a58ec55b459096a74c55fe6
Franklin Bado <fbado66@gmail.com>
2020-07-14 04:00:00 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,7 +1,7 @@
 # Write your code here
 
 # count = 10
-# while count > 0 
+# while count > 0 do
 # puts count 
 # count = count - 1 
 # end 
@@ -9,7 +9,7 @@
 
 
 count = 10 
-while count > 0 do
+while count >= 1 do
 puts count
 count -= 1
 end 
